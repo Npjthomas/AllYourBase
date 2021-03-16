67c3f45ff044158654e4bd8af8ded77db5bbc676
Npjthomas-Work <80784561+Npjthomas@users.noreply.github.com>
2021-03-16 13:09:24 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#2021-03-16 Npjthomas: initial commit
+
+# The purpose of this program is to act as a general purpose calculator for inter-base 
+# calculations. The calculator allows for basic mathematical operators (+-*/), while 
+# taking inputs and producing outputs for bases specified prior to runnning. The 
+# calculator also supports radix numbers.
